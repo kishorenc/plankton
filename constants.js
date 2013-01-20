b104c28917d0062763b1fa7f9a5033e2dfb0dd2f
Kishore Nallan <kishore@kishorelive.com>
2013-01-20 16:59:28 +0530

    1. Don't publish a post whose published property is set to false 2. Constantize property look ups on the post object.

--- constants.js
+++ constants.js
@@ -4,7 +4,9 @@ module.exports = {
 
     PERMALINK_PROPERTY: 'permalink',
     BODY_PROPERTY: 'body',
+    LAYOUT_PROPERTY: 'layout',
     DATE_PROPERTY: 'date',
+    PUBLISHED_PROPERTY: 'published',
     TIMESTAMP_GENERATED_PROPERTY: 'timestamp',
 
     SLUG_DAY: ':day',
